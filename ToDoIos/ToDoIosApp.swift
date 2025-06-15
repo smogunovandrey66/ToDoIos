ddbdef8485a15b70a4939435102516155a716b23
Andrey <smogunovandrey66@mail.ru>
2025-06-15 11:58:31 +0500

    Initial Commit

---
+++ ToDoIos/ToDoIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoIosApp.swift
+//  ToDoIos
+//
+//  Created by MacMy on 15.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
